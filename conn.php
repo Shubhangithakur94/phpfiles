c4db426e3821b50528e48b2417765a54a5622540
Shubhangi thakur <99044867+Shubhangithakur94@users.noreply.github.com>
2022-03-03 22:31:49 +0530

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+$server = "127.0.0.1"; //localhost
+$user = "root";
+$password = "";
+$db = "dbms";
+
+$conn = mysqli_connect($server,$user,$password,$db );
+// if($conn){
+//     echo "connection Successful";
+// }
+// else{
+//     echo "connection Failed";
+// }
+?>
